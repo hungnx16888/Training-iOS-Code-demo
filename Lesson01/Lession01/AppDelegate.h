d958db730377769d2c39b36bc83f85dd7abd4096
Nguyen Xuan Hung <xuanhung16888@gmail.com>
2018-04-05 16:24:38 +0700

    Example

---
+++ Lesson01/Lession01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lession01
+//
+//  Created by Nguyen Xuan Hung on 4/4/18.
+//  Copyright © 2018 Nguyen Xuan Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lesson01/Lession01/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lession01
+//
+//  Created by Nguyen Xuan Hung on 4/4/18.
+//  Copyright © 2018 Nguyen Xuan Hung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
